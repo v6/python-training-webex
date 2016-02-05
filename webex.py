7bfe1ec3c1339d85e9a9f0ed2cfc4d3326cc80e4
Nathan Basanese <nbasanes@cisco.com>
2016-02-04 16:13:13 -0800

    Refactor code to use variables.

--- webex.py
+++ webex.py
@@ -4,7 +4,17 @@
 
 import subprocess
 
-if 1 == 2:
+crazy_variable = 1
+
+sane_variable = 2
+
+if crazy_variable == sane_variable:
     print ('hello world')
 else:
+    print ('thats crazy talk')
+
+
+if 5*5 == 25: 
+    print ('this is NOT crazy talk')
+else: 
     print ('thats crazy talk')
