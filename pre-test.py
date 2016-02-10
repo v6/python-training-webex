f433217973b23fda2cc9248c8c4faccdd5539cdc
Nathan Dataguake Basanese <v6@users.noreply.github.com>
2016-02-10 10:17:04 -0800

    // , Correct syntax.
      // , This had Java syntax, not Python syntax.

--- pre-test.py
+++ pre-test.py
@@ -1,9 +1,11 @@
 '''
 
 Read the following statements and tick the box next to the correct answer.
-int a = 10;
-int b = 20;
-a = b;
+
+int a = 10
+int b = 20
+a = b
+
 The new values of a and b are:
 [ ] a = 20  b = 0
 [ ] a = 20  b = 20
@@ -17,10 +19,12 @@ The new values of a and b are:
 [ ] a = 20  b = 10
 
 Consider the following program fragment.
-if (mark > 80) grade = ‘A’;
-else if (mark > 60) grade = ‘B’;
-else if (mark > 40) grade = ‘C’;
-else grade = ‘F’;
+
+if mark > 80: grade = 'A'
+else if mark > 60: grade = 'B'
+else if mark > 40: grade = 'C'
+else: grade = 'F'
+
 What is the effect of executing this code if the variable
 mark
 has the value -12?
@@ -28,11 +32,11 @@ has the value -12?
 (b) The program will output an error message.
 (c) The variable grade will be undefined.
 (d) The program will never terminate.
-(e) The variable grade will be assigned the value ‘F’.
+(e) The variable grade will be assigned the value 'F'.
 
 From http://www.eis.mdx.ac.uk/research/PhDArea/saeed/paper1.pdf: 
 
-Formal logical proofs, and therefore programs – formal logical proofs that particular computations
+'Formal logical proofs, and therefore programs – formal logical proofs that particular computations
 are possible, expressed in a formal system called a programming language – are
 utterly meaningless.
 
@@ -42,6 +46,6 @@ some meaningless conclusion. In the test the consistent group showed a pre-accep
 they are capable of seeing mathematical calculation problems in terms of rules, and can follow those
 rules wheresoever they may lead. The inconsistent group, on the other hand, looks for meaning
 where it is not. The blank group knows that it is looking at meaninglessness, and refuses to deal
-with it.
+with it.'
 
 '''
