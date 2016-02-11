8e67cdd5885db9da1c245cc4ca06900d4b11206b
Nathan Basanese <nbasanes@cisco.com>
2016-02-11 15:39:52 -0800

    Add docstring, new functionality, make code more modular.

--- webex.py
+++ webex.py
@@ -2,9 +2,28 @@
  
 # The above line is not necessary for Windows, because paths are configured globally...
 
+'''This module helps to test network connectivity.'''
+
+'''
+Ping a list of ipaddresses, in order, to determine access. 
+
+This module also will help test connectivity to Messenger servers. 
+
+Credits: Messenger Service Engineering, specifically Reggie/Nathan
+
+'''
+
 import subprocess
 
 my_command = subprocess.call(['ls', '-a'])
+
+iplist = ['192.168.1.20', '10.0.0.1', '10.0.0.5']
+
+def iptest():
+    '''Test IP addresses from an previously defined list, and display results.'''
+    for ip in iplist:
+        print (ip)
+        subprocess.call(['ping', '-c', '2', ip]) 
 
 crazy_variable = 10000000
 
