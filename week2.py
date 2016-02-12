2017ae59603e37849612b375663d3845014f5b64
Nathan Dataguake Basanese <v6@users.noreply.github.com>
2016-02-11 21:20:36 -0800

    // , Summarize the second week.
      // , This shows how to do the lesson for people who are not present.

---
+++ week2.py
@@ -0,0 +1,31 @@
+'''
+
+Demonstrate function definitions (def), lists ([]), and looping over iterables like lists (for). 
+Check https://docs.python.org/3/tutorial/controlflow.html for how to do this. 
+
+Discuss the following way to discover the software: 
+
+1. Ask the users. 
+2. Write code tests. 
+3. Write code. 
+4. Test the code.
+5. Commit the code if it works, otherwise write more code tests, code, and test the code.
+6. Deliver the product, go back to step 1. 
+
+Check http://www.informit.com/articles/article.aspx?p=1621865 for more about this. 
+
+Show how to create a file in github.com, and at the same time, 
+create a different file in your local git repository, then do git push and git pull. 
+
+Check https://help.github.com/articles/creating-new-files/ for more on this. 
+
+Show how to create and resolve merge conflicts or 'edit collisions'. 
+
+For more on this, check https://help.github.com/articles/resolving-a-merge-conflict-from-the-command-line/. 
+
+Use subprocess to test IP addresses.
+
+Show how to use help(): https://docs.python.org/2/library/functions.html#help. 
+
+Show how to create and use docstrings: https://www.python.org/dev/peps/pep-0257/
+'''
