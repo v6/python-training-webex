b65e7e965ea291e4359b9890b590180e29e09ff6
Nathan Dataguake Basanese <v6@users.noreply.github.com>
2016-02-17 20:30:28 -0800

    // , Create a file for the third week.

---
+++ week3.py
@@ -0,0 +1,12 @@
+'''
+
+This is the third week of the Python training course, in which I introduce a few new things, including the following: 
+
+ * Add Python to social media accounts. 
+ * Review Python Philosophy.
+ * Review Git commits, review Git branching.
+ * Introduce Vagrant.
+ * Play around with functions with scopes.
+ * Use a simple HTTP based Application Programming Interface with Requests.
+ 
+ '''
