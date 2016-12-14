10948d754cbaa8f701431d181d8657685adbfb25
Nathan Dataguake Basanese <v6@users.noreply.github.com>
2016-12-13 17:54:34 -0800

    // , Add more web browser control examples

--- joke.py
+++ joke.py
@@ -1,3 +1,20 @@
-'''This is a joke.'''
+'''
+    Tell jokes and visit websites
 
+    This opens several websites & tells some jokes using the 
+    webbrowser library. 
+'''
+import webbrowser
+from webbrowser import open, open_new
 print ('This is a joke, folks.')
+
+
+print ('hello guys, vagrant is not working on windows')
+
+print ('yo this auto updates')
+
+my_list = ['appl', 'android', 'windows']
+
+for platform in my_list:
+    print(platform)
+    webbrowser.open_new('https://duckduckgo.com/' + platform)
---
+++ sitechecker/sitechecker.py
@@ -0,0 +1,14 @@
+'''Check some sites to test the webbrowser library
+
+This uses the webbrowser library from 
+python.org to open a series of sites as a 
+test.
+
+--Nathan Basanese'''
+
+import webbrowser  ##  https://docs.python.org/3/library/webbrowser.html
+
+print ('checking mah sitez, yo')
+
+webbrowser.open('https://docs.python.org/3/library/webbrowser.html')
+webbrowser.open('https://market.mashape.com/ismaelc/yoda-speak')
